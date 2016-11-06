1272dba66bca4b784b933b5c641b35570874c04a
Edward Evans <evanse2@aston.ac.uk>
2016-11-06 10:48:51 +0000

    things

---
+++ Assets/Scripts/backgroundController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public class backgroundController : MonoBehaviour
+{
+
+    private GameObject buildings1, buildings2;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    buildings1 = Instantiate(buildings1, new Vector3(0f, 0f, 0f));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
